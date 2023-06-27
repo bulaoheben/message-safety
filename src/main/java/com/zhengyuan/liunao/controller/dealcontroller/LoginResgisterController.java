69d48f8fb1e40f09373b2d514cd3c1e7a1ff5b67
bulaoheben <20301164@bjtu.edu.cn>
2023-06-27 11:22:31 +0800

    删掉无用文件

--- src/main/java/com/zhengyuan/liunao/controller/dealcontroller/LoginResgisterController.java
+++ src/main/java/com/zhengyuan/liunao/controller/dealcontroller/LoginResgisterController.java
@@ -1,44 +1,19 @@
 package com.zhengyuan.liunao.controller.dealcontroller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
-import com.zhengyuan.liunao.entity.Client;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.alibaba.fastjson.JSON;
-import com.zhengyuan.liunao.entity.Admin;
-import com.zhengyuan.liunao.entity.Company;
-import com.zhengyuan.liunao.service.AdminService;
-import com.zhengyuan.liunao.service.CompanyService;
-import com.zhengyuan.liunao.service.ClientService;
-
-import cn.hutool.crypto.SecureUtil;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/Sys")
 public class LoginResgisterController {
 
-	@Autowired
-	AdminService adminService;
-
-	@Autowired
-	CompanyService companyService;
-
-	@Autowired
-	ClientService clientService;
-	
 
 	//发送24位真彩图
 	@ResponseBody
@@ -58,5 +33,4 @@ public class LoginResgisterController {
 			return "BMP 文件上传失败";
 		}
 	}
-
 }
